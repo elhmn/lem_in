d2bb4f22420b959264543c1a105cfd9a3f82c05b
BorisM <Boris@localhost.localdomain>
2014-12-21 01:18:24 -0500

    main comments edit

--- srcs/main.c
+++ srcs/main.c
@@ -15,34 +15,18 @@
 /*
 **	1- recuperer les donnees sur l'entree standard
 **	2- checker les cas sensibles d'erreur
-**	3- rechercher les chemins les plus rapides (djikstra)
-**		-stocker les nbrs de fourmis -1 chemins possible
-**		dans l'ordre croissant
-**		-identifier les chemins direct
-**		-reflechir a la priorite avec bouchon
-**	4- Find nbrs de coups optimal.
+**	3- rechercher les chemins les plus rapides par branches (djikstra or A* algorithms)
+**		-stocker les chemins possibles dans l'ordre croissant (longueur)
+**		-identifier les chemins directes (pas necessaire pour le moment)
+**		(plutard)-reflechir a la priorite avec bouchon
+**	4- Find les coups a jouer (optimal). en determinant les F_nb pow(C_nb) ensembles de coups possible.
 **	5- affichage du resultat.
 */
 
 int		main(int ac, char **av)
 {
-//	int	fd;
-//
+	av = av;
 	if (ac)	
-	{
-		ft_putendl(av[1]);
-		ft_putendl("lem-in cant treat on more than 1 file");
-	}
-	/*
-	if (ac == 1)
-		fd = 0;
-	else
-	{
-		if ((fd = open(av[0], O_RDONLY)) == -1)
-			check_errors(MALLOC, "fd", "main.c");
-		if (ac > 2)
-			ft_putendl("lem-in cant treat on more than 1 file");
-	}
-	*/
+		ft_putendl("je suis con");
 	return (0);
 }
