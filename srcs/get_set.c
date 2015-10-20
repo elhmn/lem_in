c4e2a7177bfb86375365a744e79a6b34d0f9f12b
elhmn <elhmn@localhost.localdomain>
2015-10-20 08:12:02 +0200

    nothing to say

--- srcs/get_set.c
+++ srcs/get_set.c
@@ -1,6 +1,9 @@
 #include "lem_in.h"
 #include "check_errors.h"
-#include "stdio.h"/*_DEBUG_*/
+#include <stdio.h>/*_DEBUG_*/
+#include <unistd.h>/*_DEBUG_*/
+
+# define ABS(x) (x > 0) ? x : -x
 
 // TODO checker les conditions d arret de la
 // fonction share(t_lemin *lemin, int i_p,
@@ -33,60 +36,64 @@ static void		init_set(t_lemin *lemin)
 	}
 }
 
+void		print_set(t_lemin *lemin, int a_n)
+{
+	int		i;
+
+	i = -1;
+	printf("i{\n");
+	while (++i < lemin->path_nbr)
+	{
+		printf("\ta_n = [%d], lemin->set[%d].a = [%d]\n", a_n, i, lemin->set[i].a);
+	}
+	printf("}\n\n");
+}
+
+//	printf("i_p = [%d], a_n = [%d], lemin->set[%d].a = [%d]\n", i_p, a_n, i_p, lemin->set[i_p].a);
 static int		share(t_lemin *lemin, int i_p, int a_n, int max)
 {
-	int		tmp;
-
-	tmp = 0;
-	if (!(lemin))
+	usleep(100000);
+	if (!lemin)
 		check_errors(NUL, __FILE__, "lemin");
 	if (i_p >= lemin->path_nbr)
 	{
-		printf("calcul de couts :\n");/*_DEBUG_*/
-		printf("return 1 \n");/*_DEBUG_*/
-//		calcul_du_cout();
-//		keep();
-		return (0);
+		print_set(lemin, a_n);
+//		printf("i_p == [%d]\n", i_p);
+//		printf("calcul de couts :\n");/*_DEBUG_*/
+//		printf("return 0 \n");/*_DEBUG_*/
+////		calcul_du_cout();
+////		keep();
+		return (0);	
 	}
-	tmp = lemin->set[i_p].a;
-	if (i_p)
+	if (!i_p)
+		lemin->set[i_p].a = a_n;
+	else if (i_p > 0)
 		lemin->set[i_p].a = max - lemin->set[i_p - 1].a;
-	else
-		lemin->set[i_p].a = max - a_n;
-	share(lemin, i_p + 1, a_n, lemin->set[i_p].a);
-//	if (i_p
-//		lemin->set[i_p].a
-//		<= lemin->set[i_p].a)
-//	{
-//		lemin->set[i_p - 1].a = tmp;
-//		share(lemin, i_p - 1, a_n, lemin->set[i_p - 1].a);
-//		printf("return 2 \n");/*_DEBUG_*/
-//	}
-	//else if something else
-//	printf("return 2 \n");/*_DEBUG_*/
+	if (i_p > 0)
+		share(lemin, i_p + 1, a_n, lemin->set[i_p].a);
+	if (!i_p)
+		share(lemin, i_p + 1, a_n, lemin->ant_nbr);
 	return (0);
 }
 
 int				get_set(t_lemin *lemin)
 {
-	int		ants_nbr;
+	int		ant_nbr;
 //	int		path_nbr;
 
 	if (!lemin)
 		check_errors(NUL, __FILE__, "lemin");
-	ants_nbr = lemin->ant_nbr + 1;
+	ant_nbr = lemin->ant_nbr;
 //	path_nbr = lemin->path_nbr;
 	init_set(lemin);
+	ant_nbr = lemin->ant_nbr + 1;
 //	printf("ants_nbr = [%d] \n", lemin->ant_nbr);/*_DEBUG_*/
 
 //	distribution des fourmis.
 
 //	distribution par recurence avec un grand nombre de chemin ca pete
-
-	while (--ants_nbr)
-	{
-		share(lemin, 0, ants_nbr, lemin->ant_nbr);
-		printf("ants_nbr = [%d] \n", ants_nbr);/*_DEBUG_*/
-	}
+	while (--ant_nbr)
+		share(lemin, 0, ant_nbr, lemin->ant_nbr);
+//	printf("ants_nbr = [%d] \n", ant_nbr, lemin->ant_nbr);/*_DEBUG_*/
 	return (0);
 }
